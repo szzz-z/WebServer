974ddd0b24777c0032d6072fa58f78dacb20494e
szzz <shizhi1210@gmail.com>
2024-02-14 20:59:56 +0800

    Add some comments

--- code/buffer/buffer.cpp
+++ code/buffer/buffer.cpp
@@ -61,21 +61,35 @@ void Buffer::Append(const char *str, size_t len) {
 
 void Buffer::Append(const Buffer &buff) { Append(buff.Peek(), buff.ReadableBytes()); }
 
+void Buffer::EnsureWriteable(size_t len) {
+  if (WritableBytes() < len) {
+    AllocateSpace(len);
+  }
+  assert(WritableBytes() >= len);
+}
+
 auto Buffer::ReadFd(int fd, int *saveErrno) -> ssize_t {
   char buff[65535];
+  // 64KB也是TCP协议中单个IP数据包的最大理论大小（尽管实际上因为各种
+  // 原因，如MTU限制，实际使用的大小通常会小很多）。选择这个大小作为读取操作的缓冲区可以在单次系统调
+  // 用中读取尽可能多的数据，减少系统调用的次数，提高I/O效率。
+
   struct iovec iov[2];
   const size_t writable = WritableBytes();
 
-  iov[0].iov_base = BeginPtr() + write_pos_;
-  iov[0].iov_len = writable;
+  iov[0].iov_base = BeginPtr() + write_pos_;  // 指向数据的指针
+  iov[0].iov_len = writable;                  // 数据的长度
   iov[1].iov_base = buff;
   iov[1].iov_len = sizeof(buff);
 
+  /* 分散读， 保证数据全部读完 */
   const ssize_t len = readv(fd, iov, 2);
   if (len < 0) {
     *saveErrno = errno;
   } else if (static_cast<size_t>(len) <= writable) {
-    write_pos_ += len;
+    write_pos_ += len;  // 数据被直接读取到Buffer的当前可写空间中，即iov[0]所指向的空间。
+    // 因为数据完全位于Buffer的预分配空间内，所以不需要使用Append方法来追加数据。
+    // 直接通过增加write_pos_的值来更新Buffer的写指针，反映新追加的数据长度。
   } else {
     write_pos_ = buffer_.size();
     Append(buff, len - writable);
@@ -98,7 +112,7 @@ auto Buffer::BeginPtr() -> char * { return buffer_.data(); }
 
 auto Buffer::BeginPtr() const -> const char * { return buffer_.data(); }
 
-void Buffer::MakeSpace(size_t len) {
+void Buffer::AllocateSpace(size_t len) {
   if (WritableBytes() + PrependableBytes() < len) {
     buffer_.resize(write_pos_ + len + 1);
   } else {
