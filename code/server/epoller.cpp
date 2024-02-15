5670ab1af53515f2e262d5e92ad17929714d92b1
szzz <shizhi1210@gmail.com>
2024-02-15 14:46:17 +0800

    epoll

--- code/server/epoller.cpp
+++ code/server/epoller.cpp
@@ -1,1 +1,70 @@
 #include "epoller.h"
+
+// 传给epoll_create的参数512用于指定内核初始分配的事件表大小，
+// 但实际上这个参数在较新的Linux版本中已经被忽略，
+// 因为epoll实例的大小会根据需要动态调整。
+// 通过调用epoll_create函数创建一个epoll实例，
+// 并将返回的文件描述符存储在epoll_fd_成员变量中。
+Epoller::Epoller(int maxEvent) : epoll_fd_(epoll_create(512)), events_(maxEvent) {
+  assert(epoll_fd_ >= 0 && !events_.empty());
+}
+
+Epoller::~Epoller() { close(epoll_fd_); }  // 关闭epoll_fd_指向的epoll实例。
+
+// 向epoll实例中添加一个文件描述符fd，并指定关注的事件events。
+// 使用epoll_ctl函数，操作类型为EPOLL_CTL_ADD，向epoll实例注册文件描述符及其关联的事件。
+// 如果操作成功返回true，失败返回false。
+auto Epoller::AddFd(int fd, uint32_t events) -> bool {
+  if (fd < 0) {
+    return false;
+  }
+  epoll_event ev = {0};
+  ev.data.fd = fd;
+  ev.events = events;
+  return 0 == epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, fd, &ev);
+}
+
+// 修改epoll实例中已注册文件描述符的关注事件。
+// 操作过程与AddFd相似，但操作类型为EPOLL_CTL_MOD。
+// 成功或失败返回值与AddFd相同。
+auto Epoller::ModFd(int fd, uint32_t events) -> bool {
+  if (fd < 0) {
+    return false;
+  }
+  epoll_event ev = {0};
+  ev.data.fd = fd;
+  ev.events = events;
+  return 0 == epoll_ctl(epoll_fd_, EPOLL_CTL_MOD, fd, &ev);
+}
+
+// 从epoll实例中删除一个文件描述符。
+// 使用epoll_ctl，操作类型为EPOLL_CTL_DEL。
+// 成功或失败返回值与AddFd相同。
+auto Epoller::DelFd(int fd) -> bool {
+  if (fd < 0) {
+    return false;
+  }
+  epoll_event ev = {0};
+  return 0 == epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, fd, &ev);
+}
+
+// 调用epoll_wait等待事件的发生。
+// timeoutMs参数指定超时时间，单位为毫秒。
+// 如果timeoutMs为-1，则无限等待直到事件发生。
+// 返回值为就绪事件的数量。
+auto Epoller::Wait(int timeoutMs) -> int {
+  return epoll_wait(epoll_fd_, events_.data(), static_cast<int>(events_.size()), timeoutMs);
+}
+
+// 这两个函数用于访问Wait方法返回后，在events_向量中存储的事件。
+// GetEventFd返回第i个事件的文件描述符。
+// GetEvents返回第i个事件的类型（如EPOLLIN、EPOLLOUT等）。
+auto Epoller::GetEventFd(size_t i) const -> int {
+  assert(i < events_.size() && i >= 0);
+  return events_[i].data.fd;
+}
+
+auto Epoller::GetEvents(size_t i) const -> uint32_t {
+  assert(i < events_.size() && i >= 0);
+  return events_[i].events;
+}
---
+++ code/server/epoller.h
@@ -0,0 +1,30 @@
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <cassert>
+#include <cerrno>
+#include <vector>
+
+class Epoller {
+ public:
+  explicit Epoller(int maxEvent = 1024);
+
+  ~Epoller();
+
+  auto AddFd(int fd, uint32_t events) -> bool;
+
+  auto ModFd(int fd, uint32_t events) -> bool;
+
+  auto DelFd(int fd) -> bool;
+
+  auto Wait(int timeoutMs = -1) -> int;
+
+  auto GetEventFd(size_t i) const -> int;
+
+  auto GetEvents(size_t i) const -> uint32_t;
+
+ private:
+  int epoll_fd_;
+
+  std::vector<struct epoll_event> events_;
+};
